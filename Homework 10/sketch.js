b56c56bda697fe68dde94349433818ab2d731db0
maddiemadsmadison <madison1.collins@umontana.edu>
2023-11-20 17:05:38 -0700

    hgiufyojjh

--- Homework 10/sketch.js
+++ Homework 10/sketch.js
@@ -1,129 +1,146 @@
-var headX = 250;
-var headY = 100;
-var headDirection = 1;
 
-var bodyX = 200;
-var bodyY = 185;
-var bodyDirection = 3;
+varX = 50;
+varY = 50;
+var w = 87; 
+var s = 83;
+var a = 65;
+var d = 68;
 
-var hairX = 250;
-var hairY = 150;
-var hairDirection = 2;
+var shapeX = 30;
+var shapeY = 50;
+var shapeXSpeed;
+var shapeYSpeed;
 
-var size = 22;
-var count = 0;
-var sizeDirection = 2;
+var mouseShapeX;
+var mouseShapeY;
+function setup()
+{
+    createCanvas(800,800);
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    createCharacter(300,300);
+}
 
+function draw()
+{
+    background(17,135,239);
+    stroke(0);
+    fill(0);
+    
 
-function setup() {
-    createCanvas(500,600);
-  }
-  
-  function draw() {
-    background(157, 225, 242  );
-
-    //title
-    fill(96, 6, 6  );
-    textSize(size);
-    size+= sizeDirection;
-    count++;
-    if(count > 5)
-    {
-        sizeDirection *=-1;
-        count = 0;
-    }
-
-    text('Me at a Griz game!', 5, 250);
-    text('GO GRIZ!', 5, 270);
+    createBorders(25);
 
   
-    //artist (me)
     textSize(10);
-    text('Madison Collins', 370, 500);
-    
-    //head
-    fill(242, 215, 189);
-    
-    circle(headX,headY,100);
-    headX+=headDirection;
-    if(headX >= 418 || headX <= 0)
-    {
-        headDirection *= -1;
-    }
+    text("EXIT", width-50,height-50)
 
    
+    drawCharacter();
+    characterMovement();
 
-    //body
-    fill(118, 7, 31);
-    rect(headX,190,100,200);
+
     
-    headX+=headDirection;
-    if(headX >= 418|| headX <= 0)
+    fill(120,122,14);
+    rect(shapeX, 120, shapeY, 210);
+
+
+     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+    fill(120,122,389);
+    rect(shapeX, shapeY, 210);
+
+     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+
+    shapeX += shapeXSpeed;
+    shapeY += shapeYSpeed;
+
+    if(shapeX > width)
     {
-        headDirection *= -1;
+        shapeX = 0;
     }
+    if(shapeX < 0)
+    {
+        shapeX = width;
+    }
+    if(shapeY > height)
+    {
+        shapeY = 0;
+    }
+    if(shapeY < 0)
+    {
+        shapeY = height;
+    }
+
+
+    
+
    
-  
+    if(characterX > width && characterY > width-50)
+    {
+        fill(0);
+        stroke(5);
+        textSize(15);
+        text("WINNER WINNER CHICKEN DINNER!", width/2-10, height/2-50);
+    }
 
-    //triangle body
-    fill(128, 128, 128);
-    triangle(220,320,250,220,280,320);
+
+    fill(120,130,140);
+    circle(mouseShapeX, mouseShapeY, 55);
+}
+
+function characterMovement()
+{
+    // handle the keys
+    if(keyIsDown(w))
+    {
+        characterY -= 10;   
+    }
+    if(keyIsDown(s))
+    {
+        characterY += 10;   
+    }
+    if(keyIsDown(a))
+    {
+        characterX -= 10;   
+        console.log("movement: " + characterX);
+    }
+    if(keyIsDown(d))
+    {
+        characterX += 10;   
+    }
+}
+function createCharacter(x,y)
+{
+    characterX = x;
+    characterY = y;
+    console.log(characterX);
+    //character
     
-    //neck
-    fill(255, 255, 255);
-    triangle(230,190,250,151,270,190);
+   // circle(characterX,characterY,25);
+}
 
-    //eyes
-    fill(5);
-    point(230,80);
-    point(270,80);
+function drawCharacter()
+{
+    fill(23,40,123);
+    circle(characterX,characterY,25);
+}
+function createBorders(thickness)
+{
+    // top border
+    rect(0,0,width,thickness);
+    // left border
+    rect(0,0,thickness,height);
+    // bottom border
+    rect(0, height-thickness,width, thickness);
+    // right upper border
+    rect(width-thickness,0,thickness,height-50);
+}
 
-    //arms
-    line(120, 20, 200, 195);
-    line(370, 20, 300, 195);
-
-    //mouth
-    ellipse(250,110,10,40);
-
-    //hair
-    {fill(176, 147, 54  );
-    fill(176, 147, 54  );
-    ellipse(hairX,hairY,60,40);
-
-    hairX+=hairDirection;
-    hairY+=hairDirection
-    if(hairX >= 500 || hairX <= 0)
-    {
-        hairDirection *= -1;
-    }
-    if(hairY >= 500 || hairY <= 0)
-    {
-        hairDirection *= -1;
-    }
-   
-   }
-
-    //left leg
-    fill(242, 215, 189);
-    rect(210,bodyY,30,80);
-    bodyY += bodyDirection;
-    if(bodyY <= 0 || bodyY >= 545)
-    {
-        bodyDirection *= -1;
-    }
-    //right leg
-    fill(242, 215, 189);
-    rect(260,bodyY,30,80);
-    bodyY += bodyDirection;
-    if(bodyY <= 0 || bodyY >= 545)
-    {
-        bodyDirection *= -1;
-    }
-  
-   
-    
-
-
-    
-  }
-  
+function mouseClicked()
+{
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
