fdac316e85e0fe51335f5998c375fb8b418f6cd7
maddiemadsmadison <madison1.collins@umontana.edu>
2023-12-15 22:31:39 -0700

    FINAL

---
+++ final/sketch.js
@@ -0,0 +1,132 @@
+
+function setup()
+{
+    createCanvas(500, 500);
+}
+
+function draw()
+{
+    background(153,204,255);
+    
+    
+
+  
+
+
+  fill(220);
+  
+  //row 1
+  fill(0,0,28)
+  square(0,0,50);
+    square(50,0,50);
+
+  square(100,0,50);
+  square(150,0,50);square(200,0,50);square(250,0,50);square(300,0,50);square(350,0,50);
+  square(400,0,50);
+  square(450,0,50);
+ 
+
+  
+  //row 2
+  fill(0,0,35)
+  square(0,50 ,50);
+    
+
+  square(60,50,50);
+
+  square(120,50,50);
+  square(180,50,50);square(240,50,50);square(300,50,50);square(360,50,50);square(420,50,50);
+    
+  square(480,50,50);
+  
+
+  //row 3
+  fill(0,0,49)
+  square(0,100 ,50);
+    
+
+  square(70,100,50);
+
+  square(140,100,50);
+  square(210,100,50);square(280,100,50);square(350,100,50);square(420,100,50);square(490,100,50);
+    
+  
+  //row 4
+  fill(0,0,64)
+ 
+  square(0,150,50);
+    
+
+  square(80,150,50);
+
+  square(160,150,50);
+  square(240,150,50);square(320,150,50);square(400,150,50);square(480,150,50);
+  
+  //row 5
+  fill(0,0,75)
+ 
+  square(0,200,50);
+    
+
+  square(90,200,50);
+
+  square(180,200,50);
+  square(270,200,50);square(360,200,50);square(450,200,50);
+
+  //row 6
+  fill(0,0,92)
+  square(0,250,50);
+    
+
+  square(100,250,50);
+
+  square(200,250,50);
+  square(300,250,50);square(400,250,50);square(500,250,50);
+
+  //row 7
+  fill(0,0,100)
+  square(0,300,50);
+    
+
+  square(110,300,50);
+
+  square(220,300,50);
+  square(330,300,50);square(440,300,50);
+
+  //row 8
+  fill(0,0,118)
+  square(0,350,50);
+    
+
+  square(120,350,50);
+
+  square(240,350,50);
+  square(360,350,50);square(480,350,50);
+
+  //row 9 
+  fill(0,0,137)
+  square(0,400,50);
+    
+
+  square(130,400,50);
+
+  square(260,400,50);
+  square(390,400,50);
+
+  //row 10
+  fill(0,0,164)
+  square(0,450,50);
+    
+
+  square(140,450,50);
+
+  square(280,450,50);
+  square(420,450,50);
+
+  fill(225,225,225);
+    textSize(22);
+    text("BLUE", 225,25 );
+  
+
+}
+
